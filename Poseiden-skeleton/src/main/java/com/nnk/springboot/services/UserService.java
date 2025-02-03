5f00d01144f86889d523484c3185bc57e2236546
abrugeilles <abrugeilles@ardans.fr>
2025-02-03 15:07:26 +0100

    Ecriture des premiers tests + corrections

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
@@ -1,16 +1,33 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+    /* VARIABLES*/
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final Logger logger = Logger.getLogger(UserService.class.getName());
 
+    /* CONSTRUCTOR */
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /* METHODS */
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
-    public User getUser(int id) {
-        return userRepository.findByid(id);
+    public Optional<User> getUser(int id) {
+        return userRepository.findById(id);
     }
 
     public void saveNewUser(User user) {
@@ -22,7 +39,7 @@ public class UserService {
     public void updateUser(User user) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
-        user.setId(id);
+        user.setId(user.getId());
         userRepository.save(user);
     }
 
