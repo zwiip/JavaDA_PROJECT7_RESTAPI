55b329f44ca88821289bd941baac62667b81a1e5
abrugeilles <abrugeilles@ardans.fr>
2025-03-21 18:54:58 +0100

    Ajout d'une différence d'affichage selon si la personne est connectée ou pas

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController
 {
 	@RequestMapping("/")
-	public String home(Model model)
+	public String home(Model model, Principal principal)
 	{
+		if(principal != null) {
+			model.addAttribute("username", principal.getName());
+		}
 		return "home";
 	}
 
