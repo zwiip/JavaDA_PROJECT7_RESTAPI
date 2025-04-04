15e07fe83378e2ac367d0454f1f3a6efc2f739e0
abrugeilles <abrugeilles@ardans.fr>
2025-04-04 15:58:20 +0200

    Ajout de la JavaDoc et des Loggers sur la sécurité

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/CustomUserDetailsService.java
@@ -13,30 +13,50 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Custom implementation of UserDetailsService to load user-specific date.
+ * Used by Spring Security to retrieve user details for authentication.
+ */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+    /* VARIABLES */
     private final UserService userService;
-
     private final Logger logger = Logger.getLogger(CustomUserDetailsService.class.getName());
 
+    /* CONSTRUCTOR */
     public CustomUserDetailsService(UserService userService) { this.userService = userService; }
 
+    /* METHODS */
+
+    /**
+     * Loads user-specific data for authentication
+     * @param username the username of the user to load.
+     * @return a UserDetails object containing user data.
+     * @throws UsernameNotFoundException if the user is not found.
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.fine("Loading user by username: " + username);
         User user = userService.findUserByUsername(username);
 
         if (user == null) {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
 
-        logger.info("User loader: " + user.getUsername());
-
+        logger.info("User loaded: " + user.getUsername());
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
     }
 
+    /**
+     * Retrieves the authorities granted to the user based on their role.
+     * @param role the role of the user.
+     * @return a list of GrantedAuthority objects representing the user's authorities
+     */
     private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        logger.fine("Retrieving authorities for role: " + role);
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        logger.info("Authorities retrieved, found: " + authorities.size());
         return authorities;
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configuration/SpringSecurityConfig.java
@@ -10,18 +10,35 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.logging.Logger;
+
+/**
+ * Configuration class for Spring Security.
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+    /* VARIABLES */
     private final CustomUserDetailsService customUserDetailsService;
+    private final Logger logger = Logger.getLogger(SpringSecurityConfig.class.getName());
 
+    /* CONSTRUCTOR */
     public SpringSecurityConfig(CustomUserDetailsService customUserDetailsService) {
         this.customUserDetailsService = customUserDetailsService;
     }
 
+    /* METHODS */
+
+    /**
+     * Configures the security filter chain for the application.
+     * This defines the app rules for the access to the URLs, the login and the logout in order to protect its endpoints.
+     * @param http the HttpSecurity object to configure.
+     * @return the configured SecurityFilterChain.
+     * @throws Exception if an error occurs during configuration.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        logger.fine("Configuring Http security.");
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
@@ -43,11 +60,25 @@ public class SpringSecurityConfig {
                 .build();
     }
 
+    /**
+     * Provides a BCryptPasswordEncoder bean for encoding password.
+     * This allows us to secure a password with the bcrypt algorithm to hash passwords in an irreversible form.
+     * @return a new BCryptPassEncoder instance.
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
+        logger.fine("Initializing the password encoder.");
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Configures and provides an AuthenticationManager bean.
+     * This object manage the authentication by checking the given information for a user in order to grant or denied the access to the app ressources..
+     * @param http the HttpSecurity object to configure.
+     * @param bCryptPasswordEncoder the password encoder to use.
+     * @return the configured AuthenticationManager.
+     * @throws Exception if an error occurs during configuration.
+     */
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http
