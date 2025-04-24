846c417e64fecd9fb01236d72e7b8fa03ccd6b04
abrugeilles <abrugeilles@ardans.fr>
2025-04-24 13:49:54 +0200

    ajout de l'annotation spring repository

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package com.nnk.springboot.repositories;
 import com.nnk.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
     public User findByUsername(String username);
 
